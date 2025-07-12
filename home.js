5790e76f648e8a13d106028bf02f8f7537b9d747
Menula-De-Silva <devmenula@gmail.com>
2025-07-12 12:35:54 +0530

    Update home.js

--- home.js
+++ home.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let isDarkMode = true;
   let recognition;
-  const API_KEY = ${{secrets.API_KEY}} 
+  const API_KEY = 'AIzaSyDYV2slkUuzJ3ddfmKFXZYm6l9VWlV3OOI'; 
 
   init();
 
